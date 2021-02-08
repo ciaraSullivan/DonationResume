b7e9434631ca3bcbb703eaead02bde2653e2b2d4
Ciara Sullivan <ciaramacksullivan@gmail.com>
2021-02-08 15:51:46 -0500

    finishing form elements of builder tags and fixing some minor functionality

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ $(function(){
     if ($('.builderTab').siblings('a').hasClass("active")) {
       $(".builderTab").siblings('a').removeClass("active");
     }
+    $('.navbar-collapse').removeClass('show');
   })
 
   $('#clearButton').click(function() {
@@ -17,5 +18,17 @@ $(function(){
     $('#previewModal').modal('show');
   })
 
+  $('[data-toggle="tooltip"]').tooltip()
+
 
 });
+
+updateList = function() {
+  var input = document.getElementById('builderFiles');
+  var output = document.getElementById('fileList');
+  var children = "";
+  for (var i = 0; i < input.files.length; ++i) {
+      children += '<li>' + input.files.item(i).name + '</li>';
+  }
+  output.innerHTML = '<ul>'+children+'</ul>';
+}
